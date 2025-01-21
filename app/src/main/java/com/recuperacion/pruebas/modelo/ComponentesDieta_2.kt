726d07baaf5170c98441e7cedec320ab3a63a292
agustinluz <agustin.luz.martin.alu@iesjulioverne.es>
2025-01-21 19:50:22 +0100

    Initial commit

---
+++ app/src/main/java/com/recuperacion/pruebas/modelo/ComponentesDieta_2.kt
@@ -0,0 +1,52 @@
+package com.recuperacion.pruebas.modelo
+
+
+import java.io.Serializable
+
+
+data class ComponenteDieta(
+    var nombre: String,
+    var tipo: TipoComponente = TipoComponente.SIMPLE,
+    var grHC_ini: Double = 0.0,
+    var grLip_ini: Double = 0.0,
+    var grPro_ini: Double = 0.0,
+    val calorias: Double
+) : Serializable {
+
+    val grHC: Double
+        get() = if (tipo.esSimpleOProcesado()) grHC_ini else ingredientes.sumOf { it.cd.grHC * it.cantidad / 100 }
+
+    val grLip: Double
+        get() = if (tipo.esSimpleOProcesado()) grLip_ini else ingredientes.sumOf { it.cd.grLip * it.cantidad / 100 }
+
+    val grPro: Double
+        get() = if (tipo.esSimpleOProcesado()) grPro_ini else ingredientes.sumOf { it.cd.grPro * it.cantidad / 100 }
+
+    val Kcal: Double
+        get() = (4 * grPro) + (4 * grHC) + (9 * grLip)
+
+    val ingredientes: MutableList<Ingrediente> = mutableListOf()
+
+    fun TipoComponente.esSimpleOProcesado() =
+        this == TipoComponente.SIMPLE || this == TipoComponente.PROCESADO
+
+    fun addIngrediente(ing: Ingrediente): Boolean {
+        return if (!ingredientes.contains(ing)) {
+            ingredientes.add(ing)
+            true
+        } else false
+    }
+
+    fun removeIngrediente(ing: Ingrediente): Boolean {
+        return ingredientes.remove(ing)
+    }
+}
+
+
+//Cantidad de un ingrediente es un % sobre la cantidadTotal de un componenteDieta
+//si no se especifica nada se supone que es 100%
+//Si un alimento no especifica su cantidadTotal, se supone 100 gr o 100 ml
+data class Ingrediente(
+    var cd: ComponenteDieta,
+    var cantidad: Double = 100.0
+) : Serializable {}
